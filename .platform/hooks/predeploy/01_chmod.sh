b08baa732c24e21529ca1ed7eb6b6d1802c23e28
unknown <213412375+yunas-son-dev@users.noreply.github.com>
2026-05-24 16:31:45 +1000

    fix: Procfile uses freshbasket-idb binary name

--- .platform/hooks/predeploy/01_chmod.sh
+++ .platform/hooks/predeploy/01_chmod.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-chmod +x /var/app/staging/application 2>/dev/null || true
+chmod +x /var/app/staging/freshbasket-idb 2>/dev/null || true
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./application
+web: ./freshbasket-idb
